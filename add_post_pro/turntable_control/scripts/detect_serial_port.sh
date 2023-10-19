f4f2c5e853fdf5975318726480e21f4cd6e94943
AzizAlmusa <abdulazizalmusa@gmail.com>
2023-10-19 19:12:45 +0300

    adjusted launch file to launch arduino and arduino driver node

---
+++ add_post_pro/turntable_control/scripts/detect_serial_port.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Use the 'ls' command to list available serial ports (adjust as needed)
+available_ports=$(ls /dev/ttyACM* 2>/dev/null)
+
+# Check if any ports were found
+if [ -n "$available_ports" ]; then
+    # Select the first available port (you can modify this logic as needed)
+    selected_port=$(echo "$available_ports" | head -n 1 | tr -d '\n')
+    
+    # Change the permissions of the selected port
+    sudo chmod a+rw "$selected_port"
+    
+    echo -n "$selected_port"  # Use -n to prevent adding a newline character
+else
+    echo -n "No available serial ports found."  # Use -n to prevent adding a newline character
+fi
